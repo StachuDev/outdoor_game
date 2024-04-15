7eaa18ee277cce07ba1f2d2db653fc7babcba7e3
StachuDev <solszakdev@gmail.com>
2024-04-15 16:26:06 +0200

    google_fonts lib and theming

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,27 @@
 import 'package:flutter/material.dart';
 import 'package:outdoor_game/screens/main_page.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+final colorScheme = ColorScheme.fromSeed(
+    brightness: Brightness.dark,
+    seedColor: Color.fromARGB(255, 6, 247, 155),
+    background: Color.fromARGB(255, 49, 66, 54));
+
+final theme = ThemeData().copyWith(
+  scaffoldBackgroundColor: colorScheme.background,
+  colorScheme: colorScheme,
+  textTheme: GoogleFonts.ubuntuCondensedTextTheme().copyWith(
+    titleSmall: GoogleFonts.ubuntuCondensed(
+      fontWeight: FontWeight.bold,
+    ),
+    titleMedium: GoogleFonts.ubuntuCondensed(
+      fontWeight: FontWeight.bold,
+    ),
+    titleLarge: GoogleFonts.ubuntuCondensed(
+      fontWeight: FontWeight.bold,
+    ),
+  ),
+);
 
 main() {
   runApp(const MyApp());
@@ -10,6 +32,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: MainPageScreen(),
     );
   }
