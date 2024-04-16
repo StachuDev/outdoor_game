e93776baaae5143254ad34b8d7547cab3a20d679
StachuDev <solszakdev@gmail.com>
2024-04-16 11:37:21 +0200

    data models and env file

---
+++ lib/data/markers.dart
@@ -0,0 +1,6 @@
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+Map<String, LatLng> markers = {
+  "home": const LatLng(53.75368125420107, 20.477536510531046),
+  "lake": const LatLng(53.75413056343759, 20.477041914181616),
+};
