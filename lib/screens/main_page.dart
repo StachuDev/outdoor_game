8c3065f4be81e805c132fbd558342a8e904401a1
StachuDev <solszakdev@gmail.com>
2024-04-16 11:41:16 +0200

    main page style

--- lib/screens/main_page.dart
+++ lib/screens/main_page.dart
@@ -34,20 +34,23 @@ class MainPageScreen extends StatelessWidget {
             fit: BoxFit.cover,
           ),
           Container(
-            padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 25),
-            child: const Column(
+            padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 30),
+            child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
                 Text(
                   "Header",
-                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                  style: Theme.of(context).textTheme.titleMedium!.copyWith(
+                      color: Theme.of(context).colorScheme.onPrimaryContainer,
+                      fontSize: 20),
                 ),
-                SizedBox(
+                const SizedBox(
                   height: 16,
                 ),
                 Text(
                   "the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable.",
-                  style: TextStyle(fontSize: 16),
+                  style: Theme.of(context).textTheme.titleSmall!.copyWith(
+                      color: Theme.of(context).colorScheme.onPrimaryContainer),
                 ),
               ],
             ),
