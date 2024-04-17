b2b4c5846ea10c8276819d4369928cd99cc14ef1
StachuDev <solszakdev@gmail.com>
2024-04-17 20:03:01 +0200

    maps with location marker widget

--- lib/widgets/map.dart
+++ lib/widgets/map.dart
@@ -2,49 +2,71 @@ import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:flutter/material.dart';
 import 'package:outdoor_game/models/place_location.dart';
 
-class Map extends StatefulWidget {
-  const Map({
+class MapWidget extends StatefulWidget {
+  const MapWidget({
     super.key,
     this.location = const PlaceLocation(
-      latitude: 53.642,
-      longitude: 21.786,
-      address: '',
-    ),
+        latitude: 53.75370396489, longitude: 20.477546169471, address: ""),
+    required this.destination,
   });
 
   final PlaceLocation location;
+  final LatLng destination;
 
   @override
-  State<Map> createState() {
+  State<MapWidget> createState() {
     return _MapsScreenState();
   }
 }
 
-class _MapsScreenState extends State<Map> {
+class _MapsScreenState extends State<MapWidget> {
+  BitmapDescriptor markerIcon = BitmapDescriptor.defaultMarker;
+
+  void addCustomIcon() {
+    BitmapDescriptor.fromAssetImage(
+            const ImageConfiguration(), "assets/images/location_icon.png")
+        .then(
+      (icon) {
+        setState(() {
+          markerIcon = icon;
+        });
+      },
+    );
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    addCustomIcon();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("Udaj się do: "),
+    return GoogleMap(
+      initialCameraPosition: CameraPosition(
+        target: LatLng(
+          widget.location.latitude,
+          widget.location.longitude,
+        ),
+        zoom: 16,
       ),
-      body: GoogleMap(
-        initialCameraPosition: CameraPosition(
-          target: LatLng(
+      markers: {
+        // Marker(
+        //   markerId: const MarkerId("m1"),
+        //   position: LatLng(
+        //     widget.location.latitude,
+        //     widget.location.longitude,
+        //   ),
+        // ),
+        Marker(
+          markerId: const MarkerId("m1"),
+          icon: markerIcon,
+          position: LatLng(
             widget.location.latitude,
             widget.location.longitude,
           ),
-          zoom: 16,
         ),
-        markers: {
-          Marker(
-            markerId: const MarkerId("m1"),
-            position: LatLng(
-              widget.location.latitude,
-              widget.location.longitude,
-            ),
-          ),
-        },
-      ),
+      },
     );
   }
 }
